5a8f5d2acef8af601ca8fde07b39ffaaae3b6527
vz77vz <32020049+vz77vz@users.noreply.github.com>
2017-10-08 16:35:01 +0800

    第三次作业1

---
+++ lab3/排序函数修改版.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+#define MAXLINES 1000
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(void *lineptr[], int left, int right, int(*comp)(void*, void*));
+int numcmp(char *, char *);
+
+int main(int argc,char *argv[])
+{
+	int nlines;
+	int numeric = 0;
+	
+	if (argc > 1 && strcmp(argv[1], "-n") == 0)
+		numeric = 1;
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+	{
+		qsort((void **)lineptr, 0, nlines - 1, (int(*)(void*, void*))(numeric? numcmp:strcmp));
+		writelines(lineptr, nlines);
+		return 0;
+	}
+	else {
+		printf("input too big to sort\n");
+		return 1;
+	}
+}
+
+void qsort(void *v[], int left, int right, int(*comp)(void*, void*))
+{
+	int i, last;
+	void swap(void *v[], int, int);
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if ((*comp)(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	qsort(v, left, last - 1, comp);
+	qsort(v, last + 1, right, comp);
+
+}
